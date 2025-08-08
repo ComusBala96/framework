5703375e1d1786ecb8fe990404c6dfd5bce01e1e
Comus Bala <comusbala96.com>
2025-08-09 01:07:46 +0530

    trans_date_time function added

--- src/Helper/Functions.php
+++ src/Helper/Functions.php
@@ -19,6 +19,16 @@ function trans_date($date)
     }
     return $carbon;
 }
+function trans_date_time($date)
+{
+    $carbon = \Carbon\Carbon::parse($date)->translatedFormat('j F, Y h:i');
+    if (app()->getLocale() == 'bn') {
+        $en = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $bn = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯'];
+        return  str_replace($en, $bn, $carbon);
+    }
+    return $carbon;
+}
 function paths()
 {
     return [
