e0870cd3e9edc9342bce2b4589680c6faddd2ce7
Comus Bala <comusbala96.com>
2025-08-09 23:44:00 +0530

    date format updated

--- src/Helper/Functions.php
+++ src/Helper/Functions.php
@@ -21,7 +21,11 @@ function trans_date($date)
 }
 function trans_date_time($date)
 {
-    $carbon = \Carbon\Carbon::parse($date)->translatedFormat('j F, Y h:i');
+    if (app()->getLocale() == 'bn') {
+        $carbon = \Carbon\Carbon::parse($date)->translatedFormat('j F, Y A h:i ');
+    } else {
+        $carbon = \Carbon\Carbon::parse($date)->translatedFormat('j F, Y h:i A');
+    }
     if (app()->getLocale() == 'bn') {
         $en = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
         $bn = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯'];
